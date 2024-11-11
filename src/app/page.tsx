8c49c3043250b625c04db1eb95fa30b040a04163
Praise Afolabi <praizafolabi@gmail.com>
2024-11-11 16:08:30 +0100

    fix/background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -93,18 +93,18 @@ const ReviewSystem = () => {
   };
 
   return (
-    <div className='mx-auto px-4 py-14 space-y-8 bg-[#fff]'>
+    <div className='mx-auto px-4 py-10 space-y-8 bg-[#fff]'>
       <div className="max-w-7xl mx-auto p-4 space-y-8">
         {/* Header Section */}
         <div className="text-center mb-8">
-          <h1 className="text-4xl font-bold mb-2">Customer Reviews</h1>
+          <h1 className="text-4xl font-bold mb-2 text-[#231f20]">Customer Reviews</h1>
           <p className="text-gray-600">Share your experience with Drip Republic</p>
         </div>
 
         {/* Form and Image Section */}
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 mb-12">
           {/* Review Form */}
-          <Card className="h-[600px] order-2 lg:order-1">
+          <Card className="lg:h-[600px] order-2 lg:order-1">
             <CardHeader>
               <CardTitle><h1 className="text-xl md:text-2xl">Write a Review</h1></CardTitle>
             </CardHeader>
@@ -193,7 +193,7 @@ const ReviewSystem = () => {
 
         {/* Display Reviews */}
         <div className="space-y-4">
-          <h1 className="text-xl font-semibold mb-4">Recent Reviews</h1>
+          <h1 className="text-xl font-semibold mb-4 text-[#231f20]">Recent Reviews</h1>
           {reviews.length > 0 ? (
           <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3'>
             {reviews.map((review) => (
