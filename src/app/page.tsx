2dcc7074f12a1992d3e299155c858b940627b69a
Praise Afolabi <praizafolabi@gmail.com>
2025-01-06 00:21:14 +0100

    feat/created footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Header from "@/components/layout/header/page"
 import Home from "@/app/(landing-page)/home/page"
 import Products from "@/app/(landing-page)/products/page"
 import Values from '@/app/(landing-page)/values/page'
+import Footer from '@/app/(landing-page)/footer/page'
 import Subscribe from '@/app/(landing-page)/Subscribe/page'
 
 const page = () => {
@@ -13,6 +14,7 @@ const page = () => {
       <Values/>
       <Products/>
       <Subscribe/>
+      <Footer/>
     </div>
   )
 }
